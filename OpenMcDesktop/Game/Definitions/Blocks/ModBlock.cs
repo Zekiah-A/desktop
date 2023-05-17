5889e0c757774798e8a1a8d603466eaf4c403d04
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2023-05-17 13:48:58 +0100

    Add modblock

---
+++ OpenMcDesktop/Game/Definitions/Blocks/ModBlock.cs
@@ -0,0 +1,19 @@
+// Generic block definition used by blocks that have come in from javascript 
+using SFML.Graphics;
+using System.Collections.Generic;
+
+namespace OpenMcDesktop.Game.Definitions.Blocks;
+
+public class ModBlock : Block
+{
+    // Texture can not be static because each mod block acts like a "fake type" created ar runtime.
+    public new Texture Texture;
+    public override Texture InstanceTexture { get; }
+    public Dictionary<string, object> Data {} // Additional properties that can be attatched to mod block.
+    
+    public ModBlock(Texture texture)
+    {
+        Texture = texture;
+        InstanceTexture = Texture;
+    }
+}
