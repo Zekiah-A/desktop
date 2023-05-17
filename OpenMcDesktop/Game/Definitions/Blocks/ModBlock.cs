bc7d17fc64955ac7cf1e34896d7725e104126461
zekiahepic <73035340+Zekiah-A@users.noreply.github.com>
2023-05-17 13:52:16 +0100

    Fix modblock def

--- OpenMcDesktop/Game/Definitions/Blocks/ModBlock.cs
+++ OpenMcDesktop/Game/Definitions/Blocks/ModBlock.cs
@@ -9,11 +9,12 @@ public class ModBlock : Block
     // Texture can not be static because each mod block acts like a "fake type" created ar runtime.
     public new Texture Texture;
     public override Texture InstanceTexture { get; }
-    public Dictionary<string, object> Data {} // Additional properties that can be attatched to mod block.
+    public Dictionary<string, object> Data { get; } // Additional properties that can be attatched to mod block.
     
     public ModBlock(Texture texture)
     {
         Texture = texture;
         InstanceTexture = Texture;
+        Data = new Dictionary<string, object>();
     }
 }
